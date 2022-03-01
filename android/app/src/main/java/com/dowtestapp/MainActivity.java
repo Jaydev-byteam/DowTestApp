0932e10d90049a8c962059581d5968402c5292c6
jaydev <jaydev@betteryou.ai>
2022-03-01 17:00:18 -0600

    Added initial react-native files

---
+++ android/app/src/main/java/com/dowtestapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dowtestapp;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "DowTestApp";
+  }
+}
